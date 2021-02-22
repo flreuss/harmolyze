e1fd913c451d5c88eaa6b46b998fa114b2f58984
Florian Reuß <flreuss@uni-potsdam.de>
2021-02-22 23:54:31 +0100

    Removed Cooley's from abcFiles

--- src/tuneBookString.js
+++ src/tuneBookString.js
@@ -21,16 +21,6 @@ V:4
  G, F, B,, "_D-7"C, | D, D, "_T"G, B,,/C,/ | D, E, B,, z |] %3
 
 X:1
-T: Cooley's
-M: 4/4
-L: 1/8
-K: Emin
-|:D2|EB{c}BA B2 EB|~B2 AB dBAG|FDAD BDAD|FDAD dAFD|
-EBBA B2 EB|B2 AB defg|afe^c dBAF|DEFD E2:|
-|:gf|eB B2 efge|eB B2 gedB|A2 FA DAFA|A2 FA defg|
-eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|
-
-X:2
 T:Wenn ich ein Vöglein wär
 %%score { ( 1 2 ) | 3 }
 L:1/4
@@ -45,5 +35,5 @@ V:1
 V:2
  D2 G | [EA] D D/C/ | x3 |] %3
 V:3
- G,,3 | C, D, D,, | G,,3 |] %3
+ "_T"G,,3 | C, D, D,, | "_T"G,,3 |] %3
 `;
