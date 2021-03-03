36a4f08714818b7fc5b70c6a9301a832a515139a
Florian Reuß <flreuss@uni-potsdam.de>
2021-03-03 22:37:26 +0100

    Introduced numberSelector for Selection of addTones and BaseNote

---
+++ components/numberSelector.js
@@ -0,0 +1,33 @@
+import { Box, RadioButtonGroup, Text } from "grommet";
+
+export default function NumberSelector({ options, value, onChange, name }) {
+  return (
+    <RadioButtonGroup
+      name={name}
+      direction="row"
+      gap="xsmall"
+      justify="between"
+      options={options.map((el) => ({
+        id: `${name}-input${el}`,
+        value: el,
+        label: el.toString(),
+      }))}
+      value={value}
+      onChange={onChange}
+    >
+      {(option, { checked, hover }) => {
+        let background;
+
+        if (checked) background = "brand";
+        else if (hover) background = "light-4";
+        else background = "light-2";
+
+        return (
+          <Box background={background} pad="xsmall" round="xsmall">
+            <Text>{option.label}</Text>
+          </Box>
+        );
+      }}
+    </RadioButtonGroup>
+  );
+}
--- components/riemannFuncSelectionDialog.js
+++ components/riemannFuncSelectionDialog.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
-import { Box, Button, Heading, Layer, Text, TextInput, Select } from "grommet";
+import { Box, Button, Heading, Layer, Text, Select } from "grommet";
 
 import RiemannFunc from "../lib/riemannFunc";
+import NumberSelector from "./numberSelector";
 
 export default function RiemannFuncSelectionDialog({
   defaultValue,
   onClose,
   target,
 }) {
-  const [riemannFunc, setRiemannFunc] = useState(defaultValue);
+  const [baseFunc, setBaseFunc] = useState(defaultValue.baseFunc);
+  const [addTones, setAddTones] = useState(defaultValue.addTones);
+  const [base, setBase] = useState(defaultValue.base);
+  const [isSecondaryDominant, setIsSecondaryDominant] = useState(
+    defaultValue.isSecondaryDominant
+  );
 
   return (
     <Layer
       position="center"
-      onClickOutside={() => onClose(riemannFunc)}
-      onEsc={() => onClose(riemannFunc)}
+      onClickOutside={() =>
+        onClose(new RiemannFunc(baseFunc, addTones, base, isSecondaryDominant))
+      }
+      onEsc={() =>
+        onClose(new RiemannFunc(baseFunc, addTones, base, isSecondaryDominant))
+      }
       target={target}
     >
       <Box pad="medium" gap="small">
@@ -24,33 +34,24 @@ export default function RiemannFuncSelectionDialog({
 
         <Text>Enter Riemann Function:</Text>
 
-        <Select
-          options={riemannFunc.validBaseFuncs}
-          value={riemannFunc.baseFunc}
-          onChange={({ option }) =>
-            setRiemannFunc(
-              new RiemannFunc(
-                option,
-                riemannFunc.addTones,
-                riemannFunc.base,
-                riemannFunc.isSecondaryDominant
-              )
-            )
-          }
+        <NumberSelector
+          options={RiemannFunc.validAddTones}
+          name="addTonesSelector"
+          value={addTones[0] || 0}
+          onChange={(event) => setAddTones([+event.target.value])}
         />
-        <TextInput
-          placeholder="base"
-          value={riemannFunc.base}
-          onChange={(event) =>
-            setRiemannFunc(
-              new RiemannFunc(
-                riemannFunc.baseFunc,
-                riemannFunc.addTones,
-                event.target.value,
-                riemannFunc.isSecondaryDominant
-              )
-            )
-          }
+
+        <Select
+          options={RiemannFunc.validBaseFuncs}
+          value={baseFunc}
+          onChange={({ option }) => setBaseFunc(option)}
+        />
+
+        <NumberSelector
+          name="baseSelector"
+          options={RiemannFunc.validBaseNotes}
+          value={base}
+          onChange={(event) => setBase(+event.target.value)}
         />
         <Box
           as="footer"
@@ -68,8 +69,9 @@ export default function RiemannFuncSelectionDialog({
               </Text>
             }
             onClick={() => {
-              // Сlose the dialog and return the value
-              onClose(riemannFunc);
+              onClose(
+                new RiemannFunc(baseFunc, addTones, base, isSecondaryDominant)
+              );
             }}
             primary
           />
