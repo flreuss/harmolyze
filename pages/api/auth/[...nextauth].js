4bd752c64da272b4bd5e5a5c106528971063b9cf
Florian Reuß <flreuss@uni-potsdam.de>
2021-03-13 12:06:59 +0100

    pass isAdmin property to session.user

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,17 +4,8 @@ import { connectToDatabase } from "../../../lib/mongodb";
 import bcrypt from "bcryptjs";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      credentials: {
-        name: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
       async authorize(credentials) {
         const { db } = await connectToDatabase();
 
@@ -23,23 +14,34 @@ export default NextAuth({
           .findOne({ _id: credentials.name });
 
         if (user) {
-          const match = await bcrypt.compare(
+          const passwordMatch = await bcrypt.compare(
             credentials.password,
             user.password
           );
 
-          return match ? {name: user._id, image: user.image} : null;
+          return passwordMatch ? user : null;
         } else {
           return null;
         }
       },
     }),
-    // ...add more providers here
   ],
   jwt: {
     signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
   },
   pages: {
     signIn: "/auth/signin",
+  },
+  callbacks: {
+    async session(session, token) {
+      session.user.isAdmin = token.isAdmin;
+      session.user.name = token.name;
+      return session;
+    },
+    async jwt(token, user) {
+      if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      if (user?._id) token.name = user._id;
+      return token;
+    },
   },
 });
