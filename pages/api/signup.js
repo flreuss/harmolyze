7b2f24b8735ed4cb5df423db1db59dc69f23ed5d
Florian Reuß <flreuss@uni-potsdam.de>
2021-03-14 20:48:15 +0100

    fixed error handling in signup API

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,26 +2,29 @@ import { connectToDatabase } from "../../lib/mongodb";
 import bcrypt from "bcryptjs";
 
 export default async (req, res) => {
-  if (req.method === "POST") {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      const { db } = await connectToDatabase();
-      const user = {
-        _id: req.body.name,
-        password: hashedPassword,
-        createdAt: new Date(),
-        image: req.body.image,
-        isAdmin: false,
-      };
-      const users = await db.collection("users").insertOne(user);
-      //201 Created
-      res.status(201).json(users.insertedId);
-    } catch (err) {
-      //500 Internal Server Error
-      if (error) res.status(500).json(err);
-    }
-  } else {
-    //405 Method Not Allowed
-    res.status(405).json({});
+  switch (req.method) {
+    case "POST":
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const { db } = await connectToDatabase();
+        const user = {
+          _id: req.body.name,
+          password: hashedPassword,
+          createdAt: new Date(),
+          image: req.body.image,
+          isAdmin: false,
+        };
+        const users = await db.collection("users").insertOne(user);
+        //201 Created
+        res.status(201).json(users.insertedId);
+      } catch (err) {
+        //500 Internal Server Error
+        console.error(err);
+        res.status(500).json(err);
+      }
+      break;
+    default:
+      //405 Method Not Allowed
+      res.status(405).json({});
   }
 };
