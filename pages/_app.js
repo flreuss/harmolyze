fc4b936e1bfe9388f1d61bfe289edf0ddec1f976
Florian Reuß <flreuss@uni-potsdam.de>
2021-03-22 13:49:21 +0100

    Added page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 export default function RiemannApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <title>Riemann App</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
